449a1088fb5543a5dff4de31a5e0cf97f483d039
John-Eddy <rahmanieddy@gmail.com>
2018-09-04 14:31:13 +0200

    Ajout de nouvelle entitées + nouveau bundles

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categories;
+use App\Form\CategoriesType;
+use App\Repository\CategoriesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/categories")
+ */
+class CategoriesController extends AbstractController
+{
+    /**
+     * @Route("/", name="categories_index", methods="GET")
+     */
+    public function index(CategoriesRepository $categoriesRepository): Response
+    {
+        return $this->render('categories/index.html.twig', ['categories' => $categoriesRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="categories_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $category = new Categories();
+        $form = $this->createForm(CategoriesType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            return $this->redirectToRoute('categories_index');
+        }
+
+        return $this->render('categories/new.html.twig', [
+            'category' => $category,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="categories_show", methods="GET")
+     */
+    public function show(Categories $category): Response
+    {
+        return $this->render('categories/show.html.twig', ['category' => $category]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="categories_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Categories $category): Response
+    {
+        $form = $this->createForm(CategoriesType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('categories_edit', ['id' => $category->getId()]);
+        }
+
+        return $this->render('categories/edit.html.twig', [
+            'category' => $category,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="categories_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Categories $category): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($category);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('categories_index');
+    }
+}
